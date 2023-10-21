7cbf4235719c0a297a3640acf950c2814945eeb2
Caknoooo <caknocomel@gmail.com>
2023-10-21 18:14:26 +0700

    feat: create simple introduction

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,16 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
-    {
-        return view('welcome_message');
-    }
+  public function index()
+  {
+    $data = array(
+      'nama' => 'M Naufal Badruttamam',
+      'usia' => 20,
+      'lokasi' => 'Surabaya',
+      'profesi' => 'Software Developer',
+      'minat' => 'Machine Learning, Data Science, and Web Development'
+    );
+
+    return view('welcome_message', $data);
+  }
 }
